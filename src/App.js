000005036e6cef485a70591be5f6ab8dd137b969
shreeyaverma <shreeyaverma80@gmail.com>
2024-08-15 00:01:47 +0530

    created component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ShopCategory from './pages/ShopCategory';
+import Shop from './pages/Shop';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category="men" />} />
+          <Route path='/womens' element={<ShopCategory category="women" />} />
+          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path='/product'
+            element={<Product />}
+          >
+            <Route path=':productId' element={<Product />} />
+          </Route>
+
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.scss";
 import logo from "../assets/logo.png";
 import cart_icon from "../assets/cart_icon.png";
 
 const Navbar = () => {
+  const [menu, setmenu] = useState("shop");
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -11,17 +12,40 @@ const Navbar = () => {
         <p>Shopper</p>
       </div>
       <ul className="nav-menu">
-        <li>
+        <li
+          onClick={() => {
+            setmenu("Shop");
+          }}
+        >
           Shop
-          <hr />
+          {menu === "shop" ? <hr /> : <></>}
         </li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li
+          onClick={() => {
+            setmenu("Mens");
+          }}
+        >
+          Men {menu === "Mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setmenu("Womens");
+          }}
+        >
+          Women {menu === "Womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setmenu("Kids");
+          }}
+        >
+          Kids {menu === "Kids" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="cart">
         <button>Login</button>
         <img src={cart_icon} alt="" />
+        <div className="cart-count">0</div>
       </div>
     </div>
   );
